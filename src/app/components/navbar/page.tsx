37de481de6f63152780ba1543451983ace49132f
octadevops <octadevops@gmail.com>
2024-10-10 17:34:50 +0530

    basharath

---
+++ src/app/components/navbar/page.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { HiOutlineBell, HiOutlineUserCircle } from "react-icons/hi2";
+import Image from "next/image";
+import Logo from "@/src/app/images/NLM LOGO.png";
+
+export default function Navbar() {
+  return (
+    <nav className="bg-slate-800 shadow-lg p-4 flex justify-between items-center">
+      <div className="text-lg font-bold">
+        <Image src={Logo} alt="Logo" width={200} height={10} className="" />
+      </div>
+      <div className="flex items-center space-x-4">
+        <input
+          type="text"
+          placeholder="Search..."
+          className="border border-gray-400 rounded-md p-2 bg-transparent bg-blur"
+        />
+        <div className="relative">
+          <button className="focus:outline-none">
+            <HiOutlineBell className="text-2xl font-semibold" />
+          </button>
+        </div>
+        <div className="relative">
+          <button className="focus:outline-none">
+            <HiOutlineUserCircle className="text-2xl font-semibold" />
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+}
---
+++ src/app/components/sidebar/page.tsx
@@ -0,0 +1,48 @@
+// components/Sidebar.js
+import Link from "next/link";
+import React from "react";
+import {
+  HiOutlineClipboardDocumentList,
+  HiOutlineUserGroup,
+  HiOutlineUsers,
+} from "react-icons/hi2";
+
+const Sidebar = () => {
+  const sidebarData = [
+    {
+      label: "Invoices",
+      path: "/invoices",
+      icon: <HiOutlineClipboardDocumentList />,
+    },
+    {
+      label: "Suppliers",
+      path: "/suppliers",
+      icon: <HiOutlineUserGroup />,
+    },
+    {
+      label: "Users",
+      path: "/users",
+      icon: <HiOutlineUsers />,
+    },
+  ];
+
+  return (
+    <aside className="w-64 bg-slate-800 min-h-screen py-4 px-6 text-slate-100">
+      <ul className="space-y-2">
+        {sidebarData.map((item, index) => (
+          <li key={item.label}>
+            <Link
+              href={item.path}
+              className="flex gap-4 p-2 hover:bg-gray-300 duration-500 ease-in-out rounded hover:text-gray-800"
+            >
+              <span className="text-xl ">{item.icon}</span>
+              {item.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </aside>
+  );
+};
+
+export default Sidebar;
